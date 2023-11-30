0ced7c6ed19dd7ed988959495b8f2d290600d5b8
somi9954 <jolh999@gmail.com>
2023-12-01 02:30:26 +0900

    기본 설정 중

--- src/layouts/front/CommonLayout.js
+++ src/layouts/front/CommonLayout.js
@@ -1,5 +1,16 @@
 import Header from '../../outlines/front/Header';
 import Footer from '../../outlines/front/Footer';
+import { Outlet } from 'react-router-dom';
 
-const CommonLayout = () => {};
+const CommonLayout = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+};
 export default CommonLayout;
---
+++ src/pages/commons/NotFound.js
@@ -0,0 +1,4 @@
+const NotFound = () => {
+  return <h1>페이지 없음</h1>;
+};
+export default NotFound;

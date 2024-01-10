000d4beab18749f6282edb59611a58ec2db11137
somi9954 <jolh999@gmail.com>
2024-01-11 03:58:13 +0900

    관리자 페이지 설정

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import { useContext } from 'react';
 import UserContext from './modules/user';
 import FrontLayout from './layouts/front/CommonLayout';
 import AdminLayout from './layouts/admin/CommonLayout';
+
+/* 클라이언트 페이지 */
 import NotFound from './pages/commons/NotFound';
 import Main from './pages/front/Main';
 import Login from './pages/front/member/Login';
 import Join from './pages/front/member/Join';
 import Logout from './pages/front/member/Logout';
 import Mypage from './pages/front/member/Mypage';
+
+/* 서버 페이지 */
+import AdminMain from './pages/admin/Main';
+import AdminConfig from './pages/admin/Config';
 
 const App = () => {
   const {
@@ -29,7 +35,8 @@ const App = () => {
 
       {/* 관리자 페이지 */}
       <Route path="/admin" element={<AdminLayout />}>
-        <Route index element={<h1>관리자 페이지...</h1>} />
+        <Route index element={<AdminMain />} />
+        <Route path="config" element={<AdminConfig />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
---
+++ src/pages/admin/Config.js
@@ -0,0 +1,4 @@
+const Config = () => {
+  return <h1>사이트 설정...</h1>;
+};
+export default Config;

96189959afe1ce5e73d6c3c72d11cd77114deefa
somi9954 <jolh999@gmail.com>
2024-02-13 15:28:25 +0900

    kakao 로그인 구현중

--- src/api/member/kakao.js
+++ src/api/member/kakao.js
@@ -1,10 +1,42 @@
+import axios from 'axios';
+
 const rest_api_key = '617d3dffcc7595d0cc813068d9e8126b';
 const redirect_uri = 'http://localhost:3000/member/kakao';
 
 const auth_code_path = `https://kauth.kakao.com/oauth/authorize`;
 
+const access_token_url = 'https://kauth.kakao.com/oauth/token';
+
 export const getKakaoLoginLink = () => {
-  const kakaoURL = `${auth_code_path}?client_id=${rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
+  const kakaoURL = `${auth_code_path}?response_type=code&client_id=${rest_api_key}&redirect_uri=${redirect_uri}`;
 
   return kakaoURL;
+};
+
+export const getAccessToken = async (authCode) => {
+  const params = new URLSearchParams();
+  params.append('grant_type', 'authorization_code');
+  params.append('client_id', rest_api_key);
+  params.append('redirect_uri', redirect_uri);
+  params.append('code', authCode);
+
+  const header = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  };
+
+  const res = await axios.post(access_token_url, params.toString(), header);
+
+  const accessToken = res.data.access_token;
+
+  return accessToken;
+};
+
+export const getMemberWithAccessToken = async (accessToken) => {
+  const REACT_APP = process.env.REACT_APP_API_URL;
+  const res = await axios.get(
+    `${REACT_APP}/api/member/kakao?accessToken=${accessToken}`,
+  );
+  return res.data;
 };
--- src/containers/member/KakaoRedirectContainer.js
+++ src/containers/member/KakaoRedirectContainer.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import {
+  getAccessToken,
+  getMemberWithAccessToken,
+} from '../../api/member/kakao';
 
 const KakaoRedirectContainer = () => {
   const [searchParams] = useSearchParams();
 
   const authCode = searchParams.get('code');
+
+  useEffect(() => {
+    getAccessToken(authCode).then((accessToken) => {
+      getMemberWithAccessToken(accessToken).then((result) => {
+        console.log('----------------------------');
+        console.log('result');
+      });
+    });
+  }, [authCode]);
   return (
     <div>
       <div>KaKao Login Redirect</div>
